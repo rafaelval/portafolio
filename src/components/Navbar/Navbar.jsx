7eb1d617f810f2f919ef3cf360dcd94ecd729661
rafael <artillero.satanas@gmail.com>
2022-11-15 00:57:04 -0500

    orgaizados algunos estilos

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,52 @@
 import { useNavigate } from "react-router-dom";
 import logo from "../../images/logo.png";
-import styles from './Navbar.module.css'
+import styles from "./Navbar.module.css";
+import { BiHomeAlt } from "react-icons/bi";
+import { MdOutlineDeveloperMode } from "react-icons/md";
+import { RiSlideshowLine } from "react-icons/ri";
+import { GrContact } from "react-icons/gr";
 
 export const Navbar = () => {
-      const navigate = useNavigate()
+  const navigate = useNavigate();
 
-function handleChangePage(page){
-      navigate(page)
-}
+  function handleChangePage(page) {
+    navigate(page);
+  }
 
   return (
     <div className={styles.contPrinc}>
-      <div>
+      <div className={styles.logo}>
         <img src={logo} alt="" />
-      </div>
-      <div className="businessCard">
+        <div className={styles.businessCard}>
         <p>DESARROLLO FRONT END</p>
       </div>
-      <div>
-        <span onClick={()=> handleChangePage('/')}>Home</span>
       </div>
-      <div>
-        <span onClick={()=> handleChangePage('/areas')}>Conocimientos</span>
-      </div>
-      <div>
-        <span onClick={()=> handleChangePage('/proyectos')}>Proyectos</span>
-      </div>
-      <div>
-        <span onClick={()=> handleChangePage('/contacto')}>contacto</span>
+      
+      <div className={styles.spans}>
+        <div>
+          <span onClick={() => handleChangePage("/")}>
+            <BiHomeAlt />
+            Home
+          </span>
+        </div>
+        <div>
+          <span onClick={() => handleChangePage("/areas")}>
+            <MdOutlineDeveloperMode />
+            Conocimientos
+          </span>
+        </div>
+        <div>
+          <span onClick={() => handleChangePage("/proyectos")}>
+            <RiSlideshowLine />
+            Proyectos
+          </span>
+        </div>
+        <div>
+          <span onClick={() => handleChangePage("/contacto")}>
+            <GrContact />
+            contacto
+          </span>
+        </div>
       </div>
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,19 @@
-import styles from './Home.module.css'
-
+import styles from "./Home.module.css";
 
 export const Home = () => {
   return (
     <div className={styles.contPrinc}>
       <div className={styles.card}>
-      <h3>Hola, soy</h3>
-      <h1>Rafael Valbuena Restrepo</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam quis, dolorum sunt illo similique tempora non doloremque maiores libero. Aut accusantium odio veritatis tempore adipisci similique necessitatibus autem velit exercitationem.</p>
+        <h3>Hola, soy</h3>
+        <h1>Rafael Valbuena Restrepo</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+          quis, dolorum sunt illo similique tempora non doloremque maiores
+          libero. Aut accusantium odio veritatis tempore adipisci similique
+          necessitatibus autem velit exercitationem.
+        </p>
       </div>
-      
-      
       <div className={styles.photo}></div>
     </div>
-  )
-}
+  );
+};
